942ff5317cb84f139e8d09530b7e9de54c5cf1cb
Leonard Genders <154572934+leonardgenders@users.noreply.github.com>
2024-06-08 23:56:29 -0400

    Update BigData_Spark.py

--- BigData_Spark.py
+++ BigData_Spark.py
@@ -98,3 +98,69 @@ wordCountSorted = wordsMoreThanOne.sortBy(lambda x: x[1], False)
 
 for row in wordCountSorted.collect():
   print(row)
+
+
+# Challenge 5: Develop a Spark program using SparkSQL and data frames to calculate the average movie rating
+# Round the rating to 4 decimals, sort the df by movieID and display the first 12 observations.
+
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.types import *
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+# load the movieRatings.csv file into a df
+movieRatingsDF = spark.read.format("csv").option("header", "false").load("movieRatings.csv")
+
+# rename cols
+movieRatingsDF = movieRatingsDF.select("_c0", "_c1", "_c2", "_c3").withColumnRenamed("_c0", "userID") \
+                                                                  .withColumnRenamed("_c1", "movieID") \
+                                                                  .withColumnRenamed("_c2", "rating") \
+                                                                  .withColumnRenamed("_c3", "timestamp")
+
+movieRatingsDF.createOrReplaceTempView("movieRatingsDFview")
+# cast movieID as integer
+# round the sum of the ratings divided by the count of ratings to four decimals, group by movieid
+avgs = spark.sql("SELECT CAST(movieID as INT) as MovieID, ROUND((SUM(rating)/COUNT(rating)),4) AS AverageRating FROM movieRatingsDFview GROUP BY movieID")
+# order by movieID and in ascending order, show the first 15 observations
+avgs.select('MovieID', 'AverageRating').orderBy('MovieID', ascending = True).show(12)
+
+
+
+# Challenge 6: Create a Spark program using SparkSQL and data frames to find the mean movie rating
+# to 4 decimals and only show movies with atleast 200 ratings, output sort descending for avg ratings
+# Use col names ("MovieName", "AverageRating", "NumberOfRatings")
+
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.types import *
+sc = SparkContext.getOrCreate()
+spark = SparkSession.builder.getOrCreate()
+
+# load the movieNames.csv file into a df
+namesDF = spark.read.format("csv").option("header", "false").load("movieNames.csv")
+# rename cols in namesDF
+namesDF = namesDF.select("_c0", "_c1", "_c2").withColumnRenamed("_c0", "movieID").withColumnRenamed("_c1", "MovieName")\
+                                                                                  .withColumnRenamed("_c2", "genre")
+namesDF.show(5)
+
+# load the movieRatings.csv file into a df
+ratingsDF = spark.read.format("csv").option("header", "false").load("movieRatings.csv")
+# rename cols in ratingsDF
+ratingsDF = ratingsDF.select("_c0", "_c1", "_c2", "_c3").withColumnRenamed("_c0", "userID") \
+                                                        .withColumnRenamed("_c1", "movieID") \
+                                                        .withColumnRenamed("_c2", "rating") \
+                                                        .withColumnRenamed("_c3", "timestamp")
+ratingsDF.show(5)
+
+
+# join names and ratings on movieID
+new_joinDF = ratingsDF.join(namesDF, "movieID", "left_outer")
+new_joinDF.createOrReplaceTempView("ratingsview")
+
+# create the avg ratings by taking the sum of the ratings / the count of the ratings and naming as AverageRating
+# create the total number of ratings by taking the count of the ratings and naming as NumberOfRatings
+# group by the MovieName (which is possible bc of join above), WHERE cannot do aggregate functions so need to use
+# HAVING COUNT instead of WHERE COUNT, can use alias 'AverageRating' for ORDER BY and complete in descending order (large to small)
+movieRatingInfoDF = spark.sql("SELECT MovieName, ROUND((SUM(rating)/COUNT(rating)), 4) AS AverageRating, COUNT(rating) AS NumberOfRatings \
+FROM ratingsview GROUP BY MovieName HAVING COUNT (rating) >= 200 ORDER BY AverageRating DESC").show()
